6b841b8887982a46c7e3f61d49c8fad0a2df71e6
Danil G <danilrip065r@gmail.com>
2022-09-16 19:23:00 +0300

    step 3 and added mui

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import Box from '@mui/material/Box';
+import { Button, TextField } from '@mui/material';
+export const TaskForm = () => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!text) return;
+
+    TasksCollection.insert({
+      text: text.trim(),
+      createdAt: new Date()
+    });
+
+    setText("");
+  };
+ 
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+    >
+    <TextField id="standard-basic" label="Standard" variant="standard" placeholder="Type to add new tasks"
+        value={text}
+        onChange={(e) => setText(e.target.value)}/>
+        <Button type="submit" variant="contained" sx={{mt:1.5, ml:1}}>Add Task</Button>
+    </Box>
+  );
+};
